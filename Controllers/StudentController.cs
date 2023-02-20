df16ed49b41ed2636aafe08bb89c5069dc904e31
unknown <johnkarlo.bar@gmail.com>
2023-02-20 17:02:16 +0800

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentProject.Models;
+
+namespace StudentProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        HRContext context;
+        public StudentController()
+        {
+            context = new HRContext();
+        }
+
+        [HttpGet]
+        [Route("GetAllStudents")]
+        public List<Student> GetStudents()
+        {
+            return context.Students.ToList<Student>();
+        }
+
+        [HttpPost]
+        [Route("AddStudent")]
+        public ActionResult AddStudent(Student student)
+        {
+            try
+            {
+                context.Students.Add(student);
+                context.SaveChanges();
+
+                return Ok(student);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("RemoveStudent")]
+        public bool UpdateStudent(Student request)
+        {
+            var student = context.Students.SingleOrDefault(x => x.Id == request.Id);
+            if (student != null)
+            {
+                student.Name = request.Name;
+                student.Course = request.Course;
+                context.SaveChanges();
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        [HttpDelete]
+        [Route("RemoveStudent")]
+        public bool DeleteStudent(int id)
+        {
+            var student = context.Students.SingleOrDefault(x => x.Id == id);
+            if (student != null)
+            {
+                context.Students.Remove(student);
+                context.SaveChanges();
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+    }
+}
---
+++ Models/HRContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentProject.Models
+{
+    public class HRContext : DbContext
+    {
+        public HRContext()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectModels;Initial Catalog=StudentDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;");
+        }
+    }
+}
